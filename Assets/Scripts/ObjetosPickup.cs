87ab2211643cc114b084f28f78af75b3bab72c36
Lucas Daniel Enrique Schniper <lucasschniper@gmail.com>
2026-05-29 10:32:15 -0300

    Termine timer y pick up

---
+++ Assets/Scripts/ObjetosPickup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjetosPickup : MonoBehaviour
+{
+    
+    public CollectibleMa manager;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.CompareTag("Player"))
+        {
+            
+            if (manager != null)
+                manager.AddCollectible();
+
+            
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public float tiempoTranscurrido = 0f;
+    public bool corriendo = true;
+    public TextMeshProUGUI timerText;
+
+    void Update()
+    {
+        if (!corriendo) return;
+
+        tiempoTranscurrido += Time.deltaTime;
+
+        if (timerText != null)
+            timerText.text = FormatearTiempo(tiempoTranscurrido);
+    }
+
+    public string FormatearTiempo(float segundos)
+    {
+        int min = (int)(segundos / 60);
+        int seg = (int)(segundos % 60);
+        return string.Format("{0:00}:{1:00}", min, seg);
+    }
+
+    public void DetenerTimer()
+    {
+        corriendo = false;
+        Debug.Log("Tiempo final: " + FormatearTiempo(tiempoTranscurrido));
+    }
+
+    public float ObtenerTiempo()
+    {
+        return tiempoTranscurrido;
+    }
+}
